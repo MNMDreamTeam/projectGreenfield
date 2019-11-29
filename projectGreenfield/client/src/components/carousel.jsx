4be25abfb9603d62e1c4acffc14e181d9ad12389
MaxDorfmanUCLA <max.dorfman1@gmail.com>
2019-11-29 14:49:23 -0700

    carousel side components shift when user interacts with the arrows. Styling is decent, still needs some work. Still need to disable the automatic movement.

--- projectGreenfield/client/src/components/carousel.jsx
+++ projectGreenfield/client/src/components/carousel.jsx
@@ -3,37 +3,101 @@ import React from 'react';
 class Carousel extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            picsLeft: ["demo_iframe.htm", "https://www.w3schools.com/images/picture.jpg", "https://www.w3schools.com/images/picture.jpg"],
+            picsRight: ["https://www.w3schools.com/images/picture.jpg", "https://www.w3schools.com/images/picture.jpg", "https://www.w3schools.com/images/picture.jpg"],
+            middle: "https://www.w3schools.com/images/picture.jpg"
+        }
+        this.rightShift = this.rightShift.bind(this);
+        this.leftShift = this.leftShift.bind(this);
     }
+
+    rightShift(){
+        var left = this.state.picsLeft;
+        var right = this.state.picsRight;
+        var mid = right.shift();
+        left.push(this.state.middle);
+        right.push(left.shift());
+        this.setState({
+            picsLeft: left,
+            picsRight: right,
+            middle: mid
+        })
+    }
+
+    leftShift(){
+        var left = this.state.picsLeft;
+        var right = this.state.picsRight;
+        var mid = left.pop();
+        left.unshift(right.pop());
+        right.unshift(this.state.middle);
+        this.setState({
+            picsLeft: left,
+            picsRight: right,
+            middle: mid
+        })
+    }
+
+
     render(){
         return (
-<div class="carousel"> 
+<div class="Row">
+{this.state.picsLeft.map(item => (
+        <div class="Col">
+            <img class="rightSide" src={item}></img>
+        </div>
+))}
+<div id="carousel" class="Col"> 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://www.w3schools.com/images/picture.jpg" alt="First slide"></img>
+      <img class="d-block w-100" src="https://www.w3schools.com/images/picture.jpg"></img>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://www.w3schools.com/images/picture.jpg" alt="Second slide"></img>
+      <img class="d-block w-100" src="https://www.w3schools.com/images/picture.jpg"></img>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://www.w3schools.com/images/picture.jpg" alt="Third slide"></img>
+      <img class="d-block w-100" src="https://www.w3schools.com/images/picture.jpg"></img>
     </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://www.w3schools.com/images/picture.jpg"></img>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://www.w3schools.com/images/picture.jpg"></img>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://www.w3schools.com/images/picture.jpg"></img>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://www.w3schools.com/images/picture.jpg"></img>
+    </div>
+
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev" onClick={this.leftShift}>
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next" onClick={this.rightShift}>
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
 </div>
 </div> 
+{this.state.picsRight.map(item => (
+        <div class="Col">
+            <img class="rightSide" src={item}></img>
+        </div>
+))}
+</div>
         )
     }
 }
