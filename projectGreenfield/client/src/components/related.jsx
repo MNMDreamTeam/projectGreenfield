bd9f2ac2e7d2f9635dc0163bd970aa84667ef1cb
Myles <mcolligan@Myless-MacBook-Pro.local>
2019-12-01 12:04:53 -0700

    fetch related products

--- projectGreenfield/client/src/components/related.jsx
+++ projectGreenfield/client/src/components/related.jsx
@@ -15,31 +15,81 @@
 import React from 'react';
 
 class Related extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       relatedProducts: []
+      // add isLoading bool?
     }
   }
 
+  componentDidMount() {
+    /*
+      Something like below -
+      will need to get related products id's, then setState with matching products
+
+      How to do the seperate calls -- Nested fetch maybe??
+    */
+
+    // fetch
+      // .then - data.json
+        // .then - fetch products list
+          // .then - setState with related products
+
+    fetch('http://3.134.102.30/products/2/related')
+    .then((res) => {
+      return res.json();
+    }).then((data) => {
+      console.log('related', data);
+      data.forEach(el => {
+        fetch(`http://3.134.102.30/products/${el}`)
+        .then((item) => {
+          return item.json();
+        }).then((itemObj) => {
+          // setState?
+          console.log(itemObj);
+        })
+      })
+      // fetch('http://3.134.102.30/products/list')
+      // .then((list) => {
+      //   return list.json();
+      // }).then((products) => {
+      //   // data = array of related id's
+      //   // products = array of product objects
+      //   console.log('related', data);
+      //   console.log('products', products);
+      // })
+    })
+  }
 
   render() {
     return (
-      <div class="card-group">
-        <div class="row">
+      <div className="container">
+        <div className="row">
+          <div id="relatedProdCarousel" className="carousel slide" data-ride="carousel">
+            <div className="carousel-inner">
+              <div className="item carousel-item active">
 
-          <div class="col-sm-6">
-            <div class="card" style={{ width: 10 + 'rem' }}>
-              <img src="/296508_116_41.jpeg" class="card-img-top"></img>
-              <div class="card-body">
-                <h6 class="card-subtitle mb-2 text-muted">category</h6>
-                <h6 class="card-text">name</h6>
-                <h6 class="card-text">price</h6>
-                <h6 class="card-text">stars</h6>
+                <div className="row">
+
+                  <div className="col-sm-4">
+                    <div className="card" style={{ width: 10 + 'rem' }}>
+                      <img src="/296508_116_41.jpeg" className="card-img-top"></img>
+                      <div className="card-body">
+                        <h6 className="card-subtitle mb-2 text-muted">category</h6>
+                        <h6 className="card-text">name</h6>
+                        <h6 className="card-text">price</h6>
+                        <h6 className="card-text">stars</h6>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
               </div>
             </div>
-          </div>
 
+          </div>
         </div>
       </div>
     )
