1c93520be01eed862bf289993c0ff8ae1c68d8b6
MaxDorfmanUCLA <max.dorfman1@gmail.com>
2019-12-01 15:49:04 -0700

    styling update for future implimentation

--- projectGreenfield/client/src/components/home.jsx
+++ projectGreenfield/client/src/components/home.jsx
@@ -10,6 +10,7 @@ render(){
     return (
         <div>
             <div class="logoBar"></div>
+            
             <div class="Row">
                 <div id="Col" class="showcase">
                     <VerticalCarousel/>
@@ -35,33 +36,38 @@ render(){
                     <div class="dropdown1">
                         <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SELECT SIZE</button>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" href="#">Small</a>
+                            <a class="dropdown-item" href="#">Medium</a>
+                            <a class="dropdown-item" href="#">Large</a>
+                            <a class="dropdown-item" href="#">XL</a>
                         </div>
                     </div>
                     <div class="dropdown2">
                         <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">1</button>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" href="#">1</a>
+                            <a class="dropdown-item" href="#">2</a>
+                            <a class="dropdown-item" href="#">3</a>
+                            <a class="dropdown-item" href="#">4</a>
+                            <a class="dropdown-item" href="#">5</a>
+                            <a class="dropdown-item" href="#">6</a>
+                            <a class="dropdown-item" href="#">7</a>
+                            <a class="dropdown-item" href="#">8</a>
+                            <a class="dropdown-item" href="#">9</a>
+                            <a class="dropdown-item" href="#">10</a>
                         </div>
                     </div><br></br>
                     <div class="dropdown3">
-                        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ADD TO BAG</button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
+                        <button class="btn btn-default" type="button" id="dropdownMenuButton" aria-haspopup="true" aria-expanded="false">ADD TO BAG +</button>
                     </div>
                     <div class="dropdown4">
                         <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Star</button>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" href="#">*</a>
+                            <a class="dropdown-item" href="#">**</a>
+                            <a class="dropdown-item" href="#">***</a>
+                            <a class="dropdown-item" href="#">****</a>
+                            <a class="dropdown-item" href="#">*****</a>
                         </div>
                     </div>
                 </div> 
