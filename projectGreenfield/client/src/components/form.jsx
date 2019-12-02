158aaa3db0dae21131f8b4beef18e6a9570b3222
Neil Crothers <nkcrothers@gmail.com>
2019-12-02 11:54:38 -0700

    working on review form.

--- projectGreenfield/client/src/components/form.jsx
+++ projectGreenfield/client/src/components/form.jsx
@@ -11,42 +11,39 @@ class Form extends React.Component {
     
     render() {
         return (
-            <form>
-                <div className='col-md-3'>
-                    <h2 for='reviews'>Review</h2>
-                    <div className='form-check'>
+            <form className="needs-validation" novalidate>
+                <div className='col-md-5'>
+                    <h2 for='reviews'>Write Your Review</h2>
+                    <h6 for='reviews'>About the {this.state.product}</h6>
+                    <div className='form-check' required>
                         <input className='form-check-input' type="radio" id="one star" name="drone" value="one star"></input>
                         <label className='form-check-label' for="one star">
                             1 Star Review - "Poor"
                         </label>
-                    </div>
-                    <div className='form-check'>
+                        <br></br>
                         <input className='form-check-input' type="radio" id="two star" name="drone" value="two star"></input>
                         <label className='form-check-label' for="two star">
                             2 Star Review - "Fair"
                         </label>
-                    </div>
-                    <div className='form-check'>
+                        <br></br>
                         <input className='form-check-input' type="radio" id="three star" name="drone" value="three star"></input>
                         <label className='form-check-label' for="three star">
                             3 Star Review - "Average"
                         </label>
-                    </div>
-                    <div className='form-check'>
+                        <br></br>
                         <input className='form-check-input' type="radio" id="four star" name="drone" value="four star"></input>
                         <label className='form-check-label' for="four star">
                             4 Star Review - "Good"
                         </label>
-                    </div>
-                    <div className='form-check'>
+                        <br></br>
                         <input className='form-check-input' type="radio" id="five star" name="drone" value="five star"></input>
                         <label className='form-check-label' for="five star">
                             5 Star Review - "Great"
                         </label>
                     </div>
                     <br></br>
-                    <div className='form check form-check-inline'>
-                        <label for='do you recommend'>Do you recommend this product?</label>
+                    <div className='form check form-check-inline' required>
+                        <label for='do you recommend'>Do you recommend this product? </label>
                         <input className='form-check-input' type='radio' name='yes' id='inline radio yes' value='yes'></input>
                         <label className='form-check-label' for='yes'>yes</label>
                         <input className='form-check-input' type='radio' name='yes' id='inline radio no' value='no'></input>
@@ -55,7 +52,7 @@ class Form extends React.Component {
                     <br></br>
                     <h5 for='characteristics'>{this.state.product}</h5>
                     <label for='size'>Size</label>
-                    <div className='form check form-check-inline'>
+                    <div className='form check form-check-inline' required>
                         <input className='form-check-input' type='radio' name='1' id='inline radio characteristics' value='1'></input>
                         <label className='form-check-label' for='1'>1: A size too small</label>
                         <input className='form-check-input' type='radio' name='2' id='inline radio characteristics' value='2'></input>
@@ -69,7 +66,7 @@ class Form extends React.Component {
                     </div>
                     <br></br>
                     <label for='width'>Width</label>
-                    <div className='form check form-check-inline'>
+                    <div className='form check form-check-inline' required>
                         <input className='form-check-input' type='radio' name='1' id='inline radio characteristics' value='1'></input>
                         <label className='form-check-label' for='1'>1: Too narrow</label>
                         <input className='form-check-input' type='radio' name='2' id='inline radio characteristics' value='2'></input>
@@ -83,7 +80,7 @@ class Form extends React.Component {
                     </div>
                     <br></br>
                     <label for='comfort'>Comfort</label>
-                    <div className='form check form-check-inline'>
+                    <div className='form check form-check-inline' required>
                         <input className='form-check-input' type='radio' name='1' id='inline radio characteristics' value='1'></input>
                         <label className='form-check-label' for='1'>1: Uncomfortable</label>
                         <input className='form-check-input' type='radio' name='2' id='inline radio characteristics' value='2'></input>
@@ -97,7 +94,7 @@ class Form extends React.Component {
                     </div>
                     <br></br>
                     <label for='quality'>Quality</label>
-                    <div className='form check form-check-inline'>
+                    <div className='form check form-check-inline' required>
                         <input className='form-check-input' type='radio' name='1' id='inline radio characteristics' value='1'></input>
                         <label className='form-check-label' for='1'>1: Poor</label>
                         <input className='form-check-input' type='radio' name='2' id='inline radio characteristics' value='2'></input>
@@ -111,7 +108,7 @@ class Form extends React.Component {
                     </div>
                     <br></br>
                     <label for='length'>Length</label>
-                    <div className='form check form-check-inline'>
+                    <div className='form check form-check-inline' required>
                         <input className='form-check-input' type='radio' name='1' id='inline radio characteristics' value='1'></input>
                         <label className='form-check-label' for='1'>1: Runs short</label>
                         <input className='form-check-input' type='radio' name='2' id='inline radio characteristics' value='2'></input>
@@ -125,12 +122,12 @@ class Form extends React.Component {
                     </div>
                     <br></br>
                     <label for='fit'>Fit</label>
-                    <div className='form check form-check-inline'>
+                    <div className='form check form-check-inline' required>
                         <input className='form-check-input' type='radio' name='1' id='inline radio characteristics' value='1'></input>
                         <label className='form-check-label' for='1'>1: Runs tight</label>
-                        <input className='form-check-input' type='radio' name='2' id='inline radio characteristics' value='2'></input>
+                        <input className='form-check-input' type='radio' name='1' id='inline radio characteristics' value='2'></input>
                         <label className='form-check-label' for='2'>2: Runs slightly tight</label>
-                        <input className='form-check-input' type='radio' name='3' id='inline radio characteristics' value='3'></input>
+                        <input className='form-check-input' type='radio' name='1' id='inline radio characteristics' value='3'></input>
                         <label className='form-check-label' for='3'>3: Perfect</label>
                         <input className='form-check-input' type='radio' name='4' id='inline radio characteristics' value='4'></input>
                         <label className='form-check-label' for='4'>4: Runs slightly long</label>
@@ -138,8 +135,14 @@ class Form extends React.Component {
                         <label className='form-check-label' for='5'>5: Runs long</label>
                     </div>
                     <br></br>
+                    <br></br>
                     <label for='summary'>Review Summary</label>
-                    <textarea className='form-control rounded-0' id='reviewSummary' row='10' col='20'>Example: Best purchase ever!</textarea>
+                    <textarea className='form-control rounded-0' id='reviewSummary' rows='2' maxLength='60' placeholder='Example: Best purchase ever!'></textarea>
+                    <br></br>
+                    <label for='summary'>Review</label>
+                    <textarea className='form-control rounded-0' id='review' rows='10' maxLength='1000' minLength='50' placeholder='Why did you like the product or not?' required></textarea>
+                    <br></br>
+                    
                 </div>
             </form>
         )
