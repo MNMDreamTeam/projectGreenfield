301456349bdbea8970aa5f4a73cf5234182dfe4e
Myles <mcolligan@Myless-MacBook-Pro.local>
2019-12-03 17:01:25 -0700

    nested fetching really working

--- projectGreenfield/client/src/components/related.jsx
+++ projectGreenfield/client/src/components/related.jsx
@@ -1,14 +1,5 @@
 /*
   Base card structure built -
-  TODO: Figure how to store all the stuff you need for the card --
-
-    Pic - /styles/products/:id/styles
-    Category - /products/:id
-    Name - /products/:id
-    Price - /products/:id/styles -- if on sale show red price
-    Rating - /reviews/:id/meta
-
-    Looks like 3 api calls to me...then store in something that will eventually become state.
 
   TODO: Some more styling to make it look super awesome.
 
@@ -23,11 +14,12 @@
 import React from 'react';
 
 class Related extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      relatedProducts: []
-      // add isLoading bool?
+      test: [{cat: 'gg'}, {pic: "fake pic"}, {thing1: "thing2"}],
+      relatedProducts: [],
+      isLoading: true
     }
     this.calcRating = this.calcRating.bind(this);
   }
@@ -49,77 +41,95 @@ class Related extends React.Component {
 
   componentDidMount() {
     let tempProductsArr = [];
-
     fetch('http://3.134.102.30/products/2/related')
       .then((res) => {
         return res.json();
       }).then((relatedIds) => {
         // console.log('---', relatedIds);
-          relatedIds.forEach(el => {
-            let tempObj = {};
-            fetch(`http://3.134.102.30/products/${el}/styles`).then(res => res.json())
-              .then(styles => {
-                // console.log('---', styles.results[0].photos[0].thumbnail_url);
-                tempObj.id = styles.product_id;
-                tempObj.price = styles.results[0].original_price;
-                tempObj.salePrice = styles.results[0].sale_price;
-                tempObj.pic = styles.results[0].photos[0].thumbnail_url;
-              }).then(next => {
-                fetch(`http://3.134.102.30/products/${el}`).then(res => res.json())
-                  .then(prods => {
-                    // console.log('---', prods);
-                    tempObj.name = prods.name;
-                    tempObj.cat = prods.category;
-                  })
-              }).then(next => {
-                fetch(`http://3.134.102.30/reviews/${el}/meta`).then(res => res.json())
+        relatedIds.forEach(el => {
+          let tempObj = {};
+          fetch(`http://3.134.102.30/products/${el}/styles`).then(res => res.json())
+            .then(styles => {
+              // console.log('---', styles.results[0].photos[0].thumbnail_url);
+              tempObj.id = styles.product_id;
+              tempObj.price = styles.results[0].original_price;
+              tempObj.salePrice = styles.results[0].sale_price;
+              tempObj.pic = styles.results[0].photos[0].thumbnail_url;
+            }).then(next => {
+              fetch(`http://3.134.102.30/products/${el}`).then(res => res.json())
+                .then(prods => {
+                  // console.log('---', prods);
+                  tempObj.name = prods.name;
+                  tempObj.cat = prods.category;
+                })
+            }).then(next => {
+              fetch(`http://3.134.102.30/reviews/${el}/meta`).then(res => res.json())
                 .then(rating => {
                   // console.log('---', rating);
                   tempObj.ratings = this.calcRating(rating.ratings);
                 })
-              })
+            }).then(next => {
               tempProductsArr.push(tempObj);
-          })
+              // console.log('---', tempProductsArr)
+            }).then(next => {
+              if (tempProductsArr.length === relatedIds.length) {
+                // console.log('---', tempProductsArr)
+                this.setState({ relatedProducts: [...tempProductsArr], isLoading: false });
+              }
+            })
+        })
       }).then(last => {
-        // set state here
-        console.log('---', tempProductsArr);
+        // console.log('---', tempProductsArr)
+        // this.setState({ relatedProducts: [...tempProductsArr], isLoading: false });
       })
   }
 
   render() {
-    // console.log('related --- ', this.state.relatedProducts);
-    return (
-      <div className="container">
-        <div className="row">
-          <div id="relatedProdCarousel" className="carousel slide" data-ride="carousel">
-            <div className="carousel-inner">
-              <div className="item carousel-item active">
-                <div className="card-group">
+    // console.log('related --- ', this.state);
+    if (this.state.isLoading) {
+      // console.log('---', this.state.relatedProducts)
+      return (
+        <div>
+          <h6>Lodaing....</h6>
+        </div>
+      )
+    } else {
+      return (
+        <div className="container">
+          {console.log('---', this.state.relatedProducts[0])}
+          <div className="row">
+            <div id="relatedProdCarousel" className="carousel slide" data-ride="carousel">
+              <div className="carousel-inner">
 
-                  <div className="row">
+                <div className="item carousel-item active">
 
-                    <div className="col-sm-4">
-                      <div className="card" style={{ width: 10 + 'rem' }}>
-                        <img src="/296508_116_41.jpeg" className="card-img-top"></img>
-                        <div className="card-body">
-                          <h6 className="card-subtitle mb-2 text-muted">category</h6>
-                          <h6 className="card-text">name</h6>
-                          <h6 className="card-text">default_price</h6>
-                          <h6 className="card-text">stars</h6>
+                  <div className="card-group">
+
+                    <div className="row">
+                      <div className="col-sm-4">
+                        <div className="card" style={{ width: 10 + 'rem' }}>
+                          <img src="/296508_116_41.jpeg" className="card-img-top"></img>
+                          <div className="card-body">
+                            <h6 className="card-subtitle mb-2 text-muted">category</h6>
+                            <h6 className="card-text">name</h6>
+                            <h6 className="card-text">default_price</h6>
+                            <h6 className="card-text">stars</h6>
+                          </div>
                         </div>
                       </div>
-                    </div>
 
+                    </div>
                   </div>
 
+
                 </div>
+
               </div>
             </div>
-
           </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
