a467999339713a4aa473ed9b1ef79cb115ba8834
Hack Reactor Student <student@hackreactor.com>
2019-12-07 14:43:33 -0700

    the flood waters rising and my heart is gonna drown

--- projectGreenfield/client/src/components/modal.jsx
+++ projectGreenfield/client/src/components/modal.jsx
@@ -4,9 +4,13 @@ class Modal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentlyViewed: [],
-      relatedItem: []
+      currentlyViewed: '',
+      relatedItem: '',
+      comparedItems: [],
+      isLoading: true
     }
+    this.closeModal = this.closeModal.bind(this);
+    this.dirtyWork = this.dirtyWork.bind(this)
   }
 
   componentDidMount() {
@@ -14,45 +18,104 @@ class Modal extends React.Component {
     let compare = fetch(`http://3.134.102.30/products/${this.props.relatedId}`).then(res => res.json());
 
     Promise.all([current, compare])
-    .then((data) => {
-      this.setState({
-        currentlyViewed: [...data[0].features],
-        relatedItem: [...data[1].features]
+      .then((data) => {
+        let compared = this.dirtyWork(data)
+        Promise.all([compared])
+          .then((details) => {
+            this.setState({
+              currentlyViewed: data[0].name,
+              relatedItem: data[1].name,
+              comparedItems: [...details[0]],
+              isLoading: false
+            })
+          })
+      })
+  }
+
+  dirtyWork(prodArray) {
+    let stringsArr = [];
+    let characteristcsArr = [];
+
+    // Yeah, so...., this is terrible and I WILL refactor it
+    prodArray.forEach((el, i) => {
+      el.features.forEach(character => {
+        let str = (character.value !== 'null') ? `${character.value} ${character.feature}` : `${character.feature}`
+        if (!stringsArr.includes(str)) {
+          stringsArr.push(str);
+          if (i === 0) {
+            characteristcsArr.push([1, str, 0]);
+          } else {
+            characteristcsArr.push([0, str, 1]);
+          }
+        } else {
+          characteristcsArr[stringsArr.indexOf(str)][2] = 1;
+        }
       })
     })
+    return characteristcsArr;
+  }
+
+  closeModal() {
+
   }
 
   render() {
-    return (
-      <div>
-        {console.log('---', this.state)}
-      </div>
-    )
+    if (this.state.isLoading) {
+      return (
+        <div>
+          <h6>Lodaing....</h6>
+        </div>
+      )
+    } else {
+      return (
+        <div data-toggle="collapse" data-target="#modal" onClick={this.closeModal}>
+          <div className="modal show fade" id="modal" tabIndex="-1" role="dialog" data-show="true" style={{ display: 'block' }}>
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h4 className="modal-title" id="modalTitle">Comparing</h4>
+                  <button type="button" className="close" data-dismiss="modal" onClick={this.closeModal}>
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-sm text-left">
+                        {this.state.currentlyViewed}
+                      </div>
+                      <div className="col-sm text-right">
+                        {this.state.relatedItem}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="container-fluid">
+                    {this.state.comparedItems.map(el =>
+                      <div className="row border-bottom">
+                        <div className="col-sm text-left">
+                          {el[0] > 0 && 
+                            <b>&#10004;</b>
+                          }
+                      </div>
+                        <div className="col-sm text-center">
+                          <em>{el[1]}</em>
+                        </div>
+                        <div className="col-sm text-right">
+                          {el[2] > 0 && 
+                            <b>&#10004;</b>
+                          }
+                      </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
 export default Modal;
-
-{/* <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal">
-          jkhsklajhdfgkajfgh
-      </button> 
-    
-    <div className="modal" id="modal" tabIndex="-1" role="dialog" data-show="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="modalTitle">Alert Title Here</h5>
-            <button type="button" class="close" data-dismiss="modal">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <p>The alert text will go here......</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>*/}
