46a77fc0e5ad9fa656508731739b45e7309008ff
MaxDorfmanUCLA <max.dorfman1@gmail.com>
2019-12-11 17:38:00 -0700

    second test passes

--- projectGreenfield/client/tests/home.test.js
+++ projectGreenfield/client/tests/home.test.js
@@ -12,9 +12,9 @@ describe('Home', () => {
     
     expect(component).toMatchSnapshot();
   });
-  // it('should render banner text correctly with given strings', () => {
-  //   const id = 1;
-  //   const component = shallow(<Home prodId={id} />);
-  //   expect(component).toMatchSnapshot();
-  // });
+  it('should render banner text correctly with given strings', () => {
+    const id = 1;
+    const component = shallow(<Home prodId={id} />);
+    expect(component).toMatchSnapshot();
+  });
 });
