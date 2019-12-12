61acc54f086dfcfe2424d3bca3584b1180358615
MaxDorfmanUCLA <max.dorfman1@gmail.com>
2019-12-12 10:08:44 -0700

    testing suite implemented

--- projectGreenfield/client/tests/home.test.js
+++ projectGreenfield/client/tests/home.test.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import Home from '../src/components/home.jsx';
 import VerticalCarousel from '../src/components/verticalCarousel';
 
 
 
-
+const clickFn = jest.fn();
 describe('Home', () => {
   it('should render correctly with no props', () => {
     const component = shallow(<Home/>);
-    
     expect(component).toMatchSnapshot();
   });
-  it('should render banner text correctly with given strings', () => {
+  it('should render carousel pictures correctly with given product id', () => {
     const id = 1;
     const component = shallow(<Home prodId={id} />);
     expect(component).toMatchSnapshot();
   });
 });
+
+describe('VerticalCarousel', () => {
+  function Fixture() {
+    return (
+      <VerticalCarousel/>
+    );
+  }
+  
+  const wrapper = mount(<Fixture />); // mount/render/shallow when applicable
+  
+  expect(wrapper.find(<VerticalCarousel/>)).toExist();
+  expect(wrapper.find('ul')).not.toExist();
+
+})
+
+// describe('VerticalCarousel', () => {
+//   it('button click should add item to bag', () => {
+//     const component = shallow(<Home onClick={clickFn} />);
+//     component
+//       .find('button#dropdownMenuButton')
+//       .simulate('click');
+//     expect(clickFn).toHaveBeenCalled();
+//   });
+// });
